566df15c55791b04cd1d29590d06218d3d219295
Alexandr Ryzhenkov <alex2505@bk.ru>
2025-07-25 23:38:42 +0300

    include create db in image mysql; add createUsers.sql;

--- dump/createUsers.sql
+++ dump/createUsers.sql
@@ -1,6 +1,18 @@
-CREATE TABLE Users (
+CREATE TABLE users (
                        id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL UNIQUE,
-                       email VARCHAR(100) NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       name VARCHAR(100) NOT NULL,
+                       password VARCHAR(100) NOT NULL,
+                       createDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       remember_me varchar(255),
+                       role int default 0
 );
+
+INSERT INTO users (`id`, `email`, `name`, `password`,`createDate`,`remember_me`, `role`) VALUES (
+                                                                                                 null,
+                                                                                                 'registration55@gmail.com',
+                                                                                                 'Sasha',
+                                                                                                 '12345',
+                                                                                                 NOW(),
+                                                                                                 '1' ,
+                                                                                                 0);
--- dump/init.sql
+++
@@ -1,9 +0,0 @@
-# CREATE DATABASE example;
-CREATE TABLE users (
-                       id int(11) primary key auto_increment,
-                       email varchar(45),
-                       password varchar(255)
-                   );
-
-
-INSERT INTO users (email, password) values ('registration@gmail.com', '12345')
